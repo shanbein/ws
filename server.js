d092a5a0823e3fbe4ff3b782c69c7dc1ec833fed
shanbei <shanbei@protonmail.com>
2018-04-22 17:06:29 +0800

    local server

---
+++ server.js
@@ -0,0 +1,4 @@
+var app = require('./store')
+var port = 3000
+
+app.listen(port)
---
+++ store.js
@@ -0,0 +1,11 @@
+var express = require('express')
+app = express()
+module.exports = app
+
+app.use(express.json())
+app.get('/:name', (req, res) => {
+  return res.send(req.params)
+})
+app.post('/', (req, res) => {
+  return res.send(req.body)
+})
